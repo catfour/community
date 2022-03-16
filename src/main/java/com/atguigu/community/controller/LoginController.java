ef9e4537107111ac69fef9aa86310ee93d7091f6
catfour <1543977012@qq.com>
2022-03-16 17:32:00 +0800

    完成验证码刷新功能

---
+++ src/main/java/com/atguigu/community/controller/LoginController.java
@@ -0,0 +1,99 @@
+package com.atguigu.community.controller;
+
+import com.atguigu.community.entity.User;
+import com.atguigu.community.service.UserService;
+import com.atguigu.community.util.ActivationStatus;
+import com.google.code.kaptcha.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+@Controller
+public class LoginController implements ActivationStatus {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
+
+    @RequestMapping(path = "/register",method = RequestMethod.GET)
+    public String getRegisterPage(){
+        return "/site/register";
+    }
+    @RequestMapping(path = "/login",method = RequestMethod.GET)
+    public String getLoginPage(){
+        return "/site/login";
+    }
+
+    @RequestMapping(path = "/register",method = RequestMethod.POST)
+    public String Register(Model model, User user){
+        Map<String, Object> map = userService.register(user);
+        if(map == null || map.isEmpty()){
+            model.addAttribute("msg","账号注册成功，已发送激活链接到您邮箱，请尽快前往激活");
+            model.addAttribute("target","/index");
+            return "/site/operate-result";
+        }else{
+
+            model.addAttribute("usernameMsg",map.get("usernameMsg"));
+            model.addAttribute("emailMsg",map.get("emailMsg"));
+            model.addAttribute("passwordMsg",map.get("passwordMsg"));
+            return "/site/register";
+        }
+    }
+
+    //http://localhost:8080/community/activation/101/code
+    @RequestMapping(path = "/activation/{id}/{code}",method = RequestMethod.GET)
+    public String activation(Model model,@PathVariable("id") int id, @PathVariable("code") String code){
+        int result = userService.activation(id,code);
+        System.out.println();
+        if(result == ACTIVATION_SUCCESS){
+            model.addAttribute("msg","激活成功，您的服务已开启！");
+            model.addAttribute("target","/login");
+        }else if(result == ACTIVATION_REPEAT){
+            model.addAttribute("msg","该账号已经激活，无需重复操作！！");
+            model.addAttribute("target","/index");
+        }else {
+            model.addAttribute("msg","激活失败，您提供的激活码无效");
+            model.addAttribute("target","/index");
+        }
+        return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha",method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        //生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        //将验证码存入session
+        session.setAttribute("kaptcha",text);
+
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+        OutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            ImageIO.write(image,"png",outputStream);
+        } catch (IOException e) {
+            logger.error("响应验证码失败" + e.getMessage());
+        }
+
+
+    }
+}
